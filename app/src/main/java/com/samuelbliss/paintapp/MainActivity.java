e16db4ad74fb2e896a5a809240cb674a974183db
Josh <jstlouis34@gmail.com>
2018-04-15 00:04:42 -0500

    Implement intents

--- app/src/main/java/com/samuelbliss/paintapp/MainActivity.java
+++ app/src/main/java/com/samuelbliss/paintapp/MainActivity.java
@@ -1,13 +1,81 @@
+
 package com.samuelbliss.paintapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    Button cameraBtn;
+    Button galleryBtn;
+    ImageView imageView;
+
+    private static final String IMAGE_DIR = "/images";
+    private static final int CAMERA = 2000;
+    private static final int GALLERY = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraBtn = (Button)findViewById(R.id.camera_btn);
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA);
+            }
+        });
+
+        galleryBtn = (Button)findViewById(R.id.gallery_btn);
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, GALLERY);
+            }
+        });
+
+        imageView = (ImageView) findViewById(R.id.iv);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+
+        if(requestCode == CAMERA){
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(image);
+        }else if (requestCode == GALLERY){
+            if (data != null){
+                Uri contentUri = data.getData();
+                try{
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentUri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
