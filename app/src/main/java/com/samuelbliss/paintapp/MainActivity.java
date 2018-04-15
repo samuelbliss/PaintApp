cf725fb7c429eb246fa6635e0d93950ec6d00d8a
Samuel <samsolo97@yahoo.com>
2018-04-15 13:57:53 -0500

    Initial

--- app/src/main/java/com/samuelbliss/paintapp/MainActivity.java
+++ app/src/main/java/com/samuelbliss/paintapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.samuelbliss.paintapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PaintActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/samuelbliss/paintapp/PaintActivity.java
@@ -0,0 +1,16 @@
+package com.samuelbliss.paintapp;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PaintActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+
+
+
+    }
+}
